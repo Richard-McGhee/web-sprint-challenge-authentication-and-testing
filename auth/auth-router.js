394c50ccfc5adf0bfab82a882a012989b789b012
richard-mcghee <richardjmcghee@yahoo.com>
2020-09-18 14:46:57 -0400

    got things working, just need to write some tests

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt')
 const router = require('express').Router();
+const jwt = require('jsonwebtoken')
 const db = require('../database/dbConfig')
+
+const secret = require('./secrets')
 
 router.post('/register', (req, res) => {
   const { username, password } = req.body
@@ -23,7 +26,12 @@ router.post('/login', (req, res) => {
   db('users').where({ username }).first()
   .then(user => {
     if(user && bcrypt.compareSync(password, user.password)){
-      // Need to add stuff here for authorization and token
+      const token = generateToken(req.body)
+
+      res.status(200).json({
+        message: "Logged in succesfully",
+        token
+      })
     } else{
       res.status(401).json({ message: "Invalid credentials" })
     }
@@ -31,7 +39,19 @@ router.post('/login', (req, res) => {
   .catch(error => {
     res.status(500).json({ error: error.message })
   })
-  // Will need to finish this out
 });
+
+function generateToken(user){
+  const payload= {
+    subect: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1hr'
+  }
+
+  return jwt.sign(payload, secret.jwtSecret, options)
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,20 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken')
+const secret = require('./secrets')
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.token
+  if(!token){
+   return res.status(500).json({ message: "no authentication provided" })
+  }
+
+  jwt.verify(req.headers.token, secret.jwtSecret, (err, decoded) => {
+    if(err){
+      res.status(401).json({ you: 'shall not pass!' });      
+    } else{
+      next()
+    }
+  })
 };
