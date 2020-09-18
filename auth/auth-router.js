b046ff0bd0b0c38f047de2428f6f6b7f49b17148
richard-mcghee <richardjmcghee@yahoo.com>
2020-09-18 12:08:42 -0400

    fleshing things out more

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,16 +1,32 @@
+const bcrypt = require('bcrypt')
 const router = require('express').Router();
 const db = require('../database/dbConfig')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const { username, password } = req.body
+
+  const hashed = bcrypt.hashSync(password, 4)
+
+  db('users').insert({ username, password: hashed })
+  .then(newID => {
+    res.status(201).send("<h1>Registered successfully!</h1>")
+  })
+  .catch(error => {
+    res.status(500).json({ error: error.message })
+  })
 });
 
 router.post('/login', (req, res) => {
-  const { username } = req.body
+  const { username, password } = req.body
 
+  
   db('users').where({ username }).first()
   .then(user => {
-
+    if(user && bcrypt.compareSync(password, user.password)){
+      // Need to add stuff here for authorization and token
+    } else{
+      res.status(401).json({ message: "Invalid credentials" })
+    }
   })
   .catch(error => {
     res.status(500).json({ error: error.message })
