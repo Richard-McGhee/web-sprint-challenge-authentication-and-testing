2c0ef65d6f366fed47c5b24af040cddc58904081
richard-mcghee <richardjmcghee@yahoo.com>
2020-09-18 11:56:20 -0400

    started making out endpoint for logging in

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
+const db = require('../database/dbConfig')
 
 router.post('/register', (req, res) => {
   // implement registration
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username } = req.body
+
+  db('users').where({ username }).first()
+  .then(user => {
+
+  })
+  .catch(error => {
+    res.status(500).json({ error: error.message })
+  })
+  // Will need to finish this out
 });
 
 module.exports = router;
